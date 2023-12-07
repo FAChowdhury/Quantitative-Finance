c439d88400f960cfab5c5197a14ccba619cfda8d
FAChowdhury <chowdhury.farhan235@gmail.com>
2023-12-07 23:16:35 +1100

    completed the http request route for stock summaries

--- backend/helpers.py
+++ backend/helpers.py
@@ -5,26 +5,22 @@ from datetime import datetime, timedelta
 stocks = ['CBA.AX', 'WBC.AX', 'COL.AX', 'WOW.AX', 'ORG.AX', 
           'BHP.AX', 'CSL.AX', 'ANZ.AX', 'NAB.AX', 'FMG.AX']
 
-def summary():
-    for stock in stocks:
-        # Define the start and end dates for fetching data
-        end_date = datetime.now()
-        start_date = end_date - timedelta(days=7)  # Fetch data for the last week (7 days)
+def summary(stock: str):
+    # Define the start and end dates for fetching data
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=7)  # Fetch data for the last week (7 days)
 
-        # Create a Ticker object using yfinance
-        ticker = yf.Ticker(stock)
+    # Create a Ticker object using yfinance
+    ticker = yf.Ticker(stock)
 
-        # Get historical data for the specified period (weekly)
-        historical_data = ticker.history(start=start_date, end=end_date, interval='1wk')
-        # Extract the last closing price
-        last_close_price = historical_data['Close'].iloc[-1] 
-        second_last_close_price = historical_data['Close'].iloc[-2]
+    # Get historical data for the specified period (weekly)
+    historical_data = ticker.history(start=start_date, end=end_date, interval='1wk')
+    # Extract the last closing price
+    last_close_price = historical_data['Close'].iloc[-1] 
+    second_last_close_price = historical_data['Close'].iloc[-2]
 
-        price_change = last_close_price - second_last_close_price
+    price_change = last_close_price - second_last_close_price
 
-        percentage_change = (last_close_price - second_last_close_price) / second_last_close_price
+    percentage_change = (last_close_price - second_last_close_price) / second_last_close_price
 
-        print(f"{stock}: Last Price: {round(last_close_price, 2)}, Change: {round(price_change, 2)}, % Change: {round(percentage_change*100, 2)}%")
-
-
-summary()
+    return {'Stock': stock, 'Last_Price': round(last_close_price, 2), 'Change': round(price_change, 2), 'Percentage_Change': round(percentage_change*100, 2)}
--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from json import dumps
+from helpers import summary
 
 app = Flask(__name__)
 
 # API Routes Below:
+@app.route("/summary/<stock>", methods=["GET"])
+def summary_stocks(stock):
+    return dumps(summary(stock))
+
 # End of API Routes
 
 if __name__ == "__main__":
