c678d1b8c0cc29a11fd1713f0e1a757d8619c5b3
FAChowdhury <chowdhury.farhan235@gmail.com>
2024-01-15 14:14:43 +1100

    completed sending request to retrieve efficient portfolio data and added circular wait animation

--- frontend/src/Components/PortfolioPage.js
+++ frontend/src/Components/PortfolioPage.js
@@ -5,6 +5,7 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import TextBtn from "./TextBtn";
 import { stockList } from "../helpers";
+import CircularProgress from '@mui/material/CircularProgress';
 // import Stack from '@mui/material/Stack';
 
 const searchStyle = {
@@ -21,14 +22,35 @@ const BtnStyle = {
 const PortfolioPage = () => {
   const [selectedStocks, setSelectedStocks] = useState([]);
 
+  const [loadingEF, setLoadingEF] = useState(false);
+
   useEffect(() => {
     stockList.sort((a,b) => a.symbol.localeCompare(b.symbol))
   }, [])
 
   const handleCreatePortfolio = () => {
+    setLoadingEF(true)
     console.log(selectedStocks)
+    let stocks = selectedStocks.map((value) => value.symbol)
+    console.log(stocks)
     // call backend to create efficient portfolio
-    
+    let path = '/buildEfficientFrontier';
+    fetch(path,{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({"Tickers": stocks})
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log('POST request successful:', result);
+      setLoadingEF(false)
+    })
+    .catch(error => {
+      console.error('Error making POST request:', error);
+      setLoadingEF(false)
+    });
   }
 
   return(
@@ -52,8 +74,13 @@ const PortfolioPage = () => {
           )}
         />
       </div>
-      <div style={BtnStyle} onClick={handleCreatePortfolio}>
-        <TextBtn text={"Create Portfolio"}/>
+      <div style={BtnStyle}>
+        <div onClick={handleCreatePortfolio}>
+          <TextBtn text={"Create Portfolio"}/>
+        </div>
+        {loadingEF && (
+          <CircularProgress sx={{marginLeft: '20px'}}/>
+        )}
       </div>
     </div>
   )
